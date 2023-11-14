948727795d49a3245b7dd390bf8342b5de7f27a7
ocarignan <olive.carignan@gmail.com>
2023-11-14 16:14:20 -0500

    add hero text (no animations)

--- translations/text.js
+++ translations/text.js
@@ -4,14 +4,26 @@ const text = {
         locale: "fr",
         localeAria: "Site en français",
         localeCap: "FR",
-        contact: "Contact us"
+        contact: "Contact us",
+        hero_1: "Brunch is a ",
+        hero_2: "creative ",
+        hero_3: "agency solidifying brands through ",
+        hero_4: "user centric ",
+        hero_5: "digital solutions, and ",
+        hero_6: "operational excellence"
     },
     fr: {
         logoAria: "Logo de studio Brunch",
         locale: "en",
         localeAria: "English site",
         localeCap: "EN",
-        contact: "Contactez-nous"
+        contact: "Contactez-nous",
+        hero_1: "Brunch est une agence ",
+        hero_2: "créative ",
+        hero_3: "qui renforce les marques grâce à des ",
+        hero_4: "solutions numériques ",
+        hero_5: "centrées sur l'utilisateur et ",
+        hero_6: "l'excellence opérationnelle"
     }
 }
 
