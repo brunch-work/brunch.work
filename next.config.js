edee9a7628b1c563b40ec955988b80a93880eb0c
Olivier Carignan <olive.carignan@gmail.com>
2025-10-02 13:37:40 -0400

    plausible setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-const path = require('path')
-module.exports = {
+const { withPlausibleProxy } = require("next-plausible");
+
+const path = require("path");
+module.exports = withPlausibleProxy()({
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
-  }
-};
+  },
+});
